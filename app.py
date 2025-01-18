f57433d31f59c7d592944553edc33846d68112ac
spawar20 <43914793+spawar20@users.noreply.github.com>
2025-01-18 22:09:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,6 @@ def calculate():
             result = "Invalid input"
     return render_template("index.html", result=result)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":.
+    port = int(os.environ.get("PORT", 5000))  # Use the port Render provides
+    app.run(host="0.0.0.0", port=port, debug=True)
